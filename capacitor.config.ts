838703d20fd82e852f25571ac65b3b2e810ba761
svatoslavarosenko <quex.modest@gmail.com>
2025-03-01 23:33:23 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'easy-hire-angular',
+  webDir: 'www'
+};
+
+export default config;
