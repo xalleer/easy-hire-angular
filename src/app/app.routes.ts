b387c3418e2f9a56d34917052975b04e0177b89f
svatoslavarosenko <quex.modest@gmail.com>
2025-03-01 23:52:12 +0200

    add: auth pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,30 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'auth',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'welcome',
+        loadComponent: () => import('./auth/welcome/welcome.component').then((m) => m.WelcomeComponent),
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login.component').then((m) => m.LoginComponent),
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./auth/register/register.component').then((m) => m.RegisterComponent),
+      },
+    ],
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'auth/login',
     pathMatch: 'full',
   },
 ];
